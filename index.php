e55b632956ae6c68a5be6885c46232041c185d8d
taufikrahman <rahmantaufikur10@gmail.com>
2019-03-05 16:19:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
-1.	Taufikur Rahman							E41172054
-2. 	Nofita Safira Anggraini					E41172160
-3.  Muhammad Hadana Sabilal Muttaqin 		E41172152
-4.	Arif Adi Kurniawan 						E41172008
+<!DOCTYPE html>
+<html>
+<head>
+	<title>KELOMPOK 4</title>
+</head>
+<body>
+<h1>
+1.	Taufikur Rahman						E41172054<br/>
+2. 	Nofita Safira Anggraini					E41172160<br/>
+3.  Muhammad Hadana Sabilal Muttaqin 		E41172152<br/>
+4.	Arif Adi Kurniawan 						E41172008<br/>
 5.
+</h1>
+</body>
+</html>
