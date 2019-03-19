ba70d5a823586013d09f54f77d091f4ad780a8ee
Nofita Safira Anggraini <nofitasafira99@gmail.com>
2019-03-19 15:21:16 +0700

    Update dashboard.php

--- CI.3.E41172160_P2/framework/application/views/admin/dashboard.php
+++ CI.3.E41172160_P2/framework/application/views/admin/dashboard.php
@@ -12,14 +12,14 @@
 
   <title>SB Admin - Dashboard</title>
 
-   <!-- Custom fonts for this template-->
-   <link href=<?php echo base_url('assets/fontawesome-free/css/all.min.css'); ?> rel="stylesheet" type="text/css">
+  <!-- Custom fonts for this template-->
+  <link href=<?php echo base_url ("assets/fontawesome-free/css/all.min.css"); ?> rel="stylesheet" type="text/css">
 
-<!-- Page level plugin CSS-->
-<link href=<?php echo base_url("assets/datatables/dataTables.bootstrap4.css"); ?> rel="stylesheet">
+  <!-- Page level plugin CSS-->
+  <link href=<?php echo base_url ("assets/datatables/dataTables.bootstrap4.css");?> rel="stylesheet">
 
-<!-- Custom styles for this template-->
-<link href=<?php echo base_url("css/sb-admin.css");?> rel="stylesheet">
+  <!-- Custom styles for this template-->
+  <link href=<?php echo base_url ("css/sb-admin.css")?> rel="stylesheet">
 
 </head>
 
@@ -750,23 +750,23 @@
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src=<?php base_url("assets/jquery/jquery.min.js");?>></script>
-  <script src=<?php base_url("assets/bootstrap/js/bootstrap.bundle.min.js");?>></script>
+  <script src="<?php echo base_url ('assets/jquery/jquery.min.js')?>"></script>
+  <script src="<?php echo base_url ('assets/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src=<?php base_url("assets/jquery-easing/jquery.easing.min.js");?>></script>
+  <script src="<?php echo base_url ('assets/jquery-easing/jquery.easing.min.js')?>"></script>
 
   <!-- Page level plugin JavaScript-->
-  <script src=<?php base_url("assets/chart.js/Chart.min.js");?>></script>
-  <script src=<?php base_url("assets/datatables/jquery.dataTables.js");?>></script>
-  <script src=<?php base_url("assets/datatables/dataTables.bootstrap4.js");?>></script>
+  <script src="<?php echo base_url ('assets/chart.js/Chart.min.js')?>"></script>
+  <script src="<?php echo base_url ('assets/datatables/jquery.dataTables.js')?>"></script>
+  <script src="<?php echo base_url ('assets/datatables/dataTables.bootstrap4.js')?>"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src=<?php base_url("js/sb-admin.min.js");?>></script>
+  <script src="<?php echo base_url ('js/sb-admin.min.js')?>"></script>
 
   <!-- Demo scripts for this page-->
-  <script src=<?php base_url("js/demo/datatables-demo.js");?>></script>
-  <script src=<?php base_url("js/demo/chart-area-demo.js");?>></script>
+  <script src="<?php echo base_url ('js/demo/datatables-demo.js')?>"></script>
+  <script src="<?php echo base_url ('js/demo/chart-area-demo.js')?>"></script>
 
 </body>
 
