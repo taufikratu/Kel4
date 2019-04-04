2d4c01291e80247772812559929e7282aad064aa
Nofita Safira Anggraini <nofitasafira99@gmail.com>
2019-04-04 15:49:58 +0700

    Update v_barang.php

--- CI.3.E41172160_P2/application/views/v_barang.php
+++ CI.3.E41172160_P2/application/views/v_barang.php
@@ -10,7 +10,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin - Dashboard</title>
+  <title>Table</title>
 
   <!-- Custom fonts for this template-->
   <link href=<?php echo base_url ("assets/fontawesome-free/css/all.min.css"); ?> rel="stylesheet" type="text/css">
