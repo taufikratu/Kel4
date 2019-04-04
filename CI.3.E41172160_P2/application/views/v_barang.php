803585f9dc03d33b1a1a536357c9bf85499f61c4
Nofita Safira Anggraini <nofitasafira99@gmail.com>
2019-04-04 16:49:05 +0700

    Update v_barang.php

--- CI.3.E41172160_P2/application/views/v_barang.php
+++ CI.3.E41172160_P2/application/views/v_barang.php
@@ -272,7 +272,7 @@
 
   <!-- Scroll to Top Button-->
   <a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
+    <i class="fas fa-angle-up"></i> 
   </a>
 
   <!-- Logout Modal-->
