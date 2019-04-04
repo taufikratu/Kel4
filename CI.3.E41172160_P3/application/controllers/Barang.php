874bc793fcb59aae03aa64fed740f5b805127aeb
Nofita Safira Anggraini <nofitasafira99@gmail.com>
2019-04-04 15:28:19 +0700

    menampilkan table

---
+++ CI.3.E41172160_P3/application/controllers/Barang.php
@@ -0,0 +1,16 @@
+<?php
+
+class Barang extends CI_Controller{
+
+    function index(){
+        $this->load->model("m_barang");
+        $nama="Judul Barang";
+        $data['judul']=$nama;
+        $data['item']=$this->m_barang->list_barang();
+        $this->load->view("v_barang",$data);
+    }
+
+function edit(){
+    echo"Edit";
+}
+}
---
+++ CI.3.E41172160_P3/application/models/m_barang.php
@@ -0,0 +1,10 @@
+<?php
+
+class m_barang extends CI_Model{
+    private $_table="barang";
+
+    public function list_barang(){
+       return $this->db->get($this->_table)->result();
+    }
+}
+?>
