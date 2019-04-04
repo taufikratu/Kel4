8a3a387b0ad98fca67654a38d0da7640168c5ca0
Nofita Safira Anggraini <nofitasafira99@gmail.com>
2019-04-04 14:28:27 +0700

    edit

--- CI.3.E41172160_P2/application/config/config.php
+++ CI.3.E41172160_P2/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/framework';
+$config['base_url'] = 'http://localhost/Kel4/CI.3.E41172160_P2';
 
 /*
 |--------------------------------------------------------------------------
---
+++ CI.3.E41172160_P2/application/views/v_barang.php
@@ -0,0 +1,9 @@
+<?php
+foreach($list as $row){
+    echo $row->kdbarang."<br>";
+    echo $row->nama."<br>";
+    echo $row->deskripsi."<br>";
+    echo $row->stokbarang."<br>";
+    echo $row->hargabarang."<br>";
+}
+?>
