0b32e595968df3e36692df4af444ca8761e68afb
Nofita Safira Anggraini <nofitasafira99@gmail.com>
2019-05-15 15:30:45 +0700

    2

---
+++ sablon/application/controllers/user/Login.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+  public function index()
+  {
+    $this->load->view('user/login');   
+  }
+
+}
+
+/* End of file Register.php */
+/* Location: ./application/controllers/admin/Register.php */
