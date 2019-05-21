7fd43179db97acbfcffb7184affd12c861815502
Nofita Safira Anggraini <nofitasafira99@gmail.com>
2019-05-21 10:26:28 +0700

    update about

--- sablon/application/views/about.php
+++ sablon/application/views/about.php
@@ -1,31 +1,166 @@
-<div class="row section featured topspace">
-      <h2 class="section-title"><span>About</span></h2>
-      <div class="contact-page-side-content">
-						<h3 class="contact-page-title">Contact Us</h3>
-                        <p class="contact-page-message mb-30">Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram anteposuerit litterarum formas human.</p>
-						<!--=======  single contact block  =======-->
-						
-						<div class="single-contact-block">
-							<h4><i class="fa fa-fax"></i> Address</h4>
-							<p>123 Main Street, Anytown, CA 12345 – USA</p>
-						</div>
-						
-						<!--=======  End of single contact block  =======-->
 
-						<!--=======  single contact block  =======-->
-						
-						<div class="single-contact-block">
-							<h4><i class="fa fa-phone"></i> Phone</h4>
-							<p>Mobile: (08) 123 456 789</p>
-							<p>Hotline: 1009 678 456</p>
-						</div>
-						
-						<!--=======  End of single contact block  =======-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author"      content="Sergey Pozhilov (GetTemplate.com)">
 
-						<!--=======  single contact block  =======-->
-						
-						<div class="single-contact-block">
-							<h4><i class="fa fa-envelope-o"></i> Email</h4>
-							<p>yourmail@domain.com</p>
-							<p>support@hastech.company</p>
-						</div>
+	<title>About page | Initio - Free, multipurpose html5 template by GetTemplate</title>
+
+	<link rel="shortcut icon" href="assets/images/gt_favicon.png">
+
+	<!-- Bootstrap -->
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.no-icons.min.css" rel="stylesheet">
+	<!-- Icon font -->
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	<!-- Fonts -->
+	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Alice|Open+Sans:400,300,700">
+	<!-- Custom styles -->
+	<link rel="stylesheet" href="assets/css/styles.css">
+
+	<!--[if lt IE 9]> <script src="assets/js/html5shiv.js"></script> <![endif]-->
+</head>
+<body>
+<!-- Sidebar -->
+<aside class="col-sm-4 sidebar sidebar-right">
+
+<ul class="nav text-right nav-side">
+	<li class="active"><a href="#ui">Tentang Toko</a></li>
+	<li><a href="#frontend">Cara Order</a></li>
+</ul>
+
+</aside>
+<!-- /Sidebar -->
+
+			</div><!--/.nav-collapse -->
+		</div>
+	</nav>
+</header>
+
+<main id="main">
+
+	<div class="container">
+
+		<div class="row topspace">
+
+			<!-- Article main content -->
+			<article class="col-sm-8 maincontent">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, consequuntur eius repellendus eos aliquid molestiae ea laborum ex quibusdam laudantium voluptates placeat consectetur quam aliquam beatae soluta accusantium iusto nihil nesciunt unde veniam magnam repudiandae sapiente.</p>
+				<p><img src="assets/images/mac.jpg" alt="" class="" width="100%" ></p>
+				<h3>Necessitatibus</h3>
+				<p>Velit, odit, eius, libero unde impedit quaerat dolorem assumenda alias consequuntur optio quae maiores ratione tempore sit aliquid architecto eligendi pariatur ab soluta doloremque dicta aspernatur labore quibusdam dolore corrupti quod inventore. Maiores, repellat, consequuntur eius repellendus eos aliquid molestiae ea laborum ex quibusdam laudantium voluptates placeat consectetur quam aliquam!</p>
+				<h3>Fugit, laboriosam</h3>
+				<p>Eum, quasi, est, vitae, ipsam nobis consectetur ea aspernatur ad eos voluptatibus fugiat nisi perferendis impedit. Quam, nulla, excepturi, voluptate minus illo tenetur sint ab in culpa cumque impedit quibusdam. Saepe, molestias quia voluptatem natus velit fugiat omnis rem eos sapiente quasi quaerat aspernatur quisquam deleniti accusantium laboriosam odio id?</p>
+			</article>
+			<!-- /Article -->
+
+
+		</div>
+	</div>	<!-- /container -->
+
+</main>
+
+<footer id="footer" class="topspace">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 widget">
+				<h3 class="widget-title">Contact</h3>
+				<div class="widget-body">
+					<p>+234 23 9873237<br>
+					<a href="mailto:#">khairasablon@gmail.com</a><br>
+						<br>
+						234 Hidden Pond Road, Ashland City, TN 37015
+					</p>
+				</div>
+			</div>
+
+			<div class="col-md-3 widget">
+				<h3 class="widget-title">Follow me</h3>
+				<div class="widget-body">
+					<p class="follow-me-icons">
+						<a href=""><i class="fa fa-twitter fa-2"></i></a>
+						<a href=""><i class="fa fa-dribbble fa-2"></i></a>
+						<a href=""><i class="fa fa-github fa-2"></i></a>
+						<a href=""><i class="fa fa-facebook fa-2"></i></a>
+					</p>
+				</div>
+			</div>
+
+			<div class="col-md-3 widget">
+				<h3 class="widget-title">Text widget</h3>
+				<div class="widget-body">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, nihil natus explicabo ipsum quia iste aliquid repellat eveniet velit ipsa sunt libero sed aperiam id soluta officia asperiores adipisci maxime!</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, nihil natus explicabo ipsum quia iste aliquid repellat eveniet velit ipsa sunt libero sed aperiam id soluta officia asperiores adipisci maxime!</p>
+				</div>
+			</div>
+
+			<div class="col-md-3 widget">
+				<h3 class="widget-title">Form widget</h3>
+				<div class="widget-body">
+					<p>+234 23 9873237<br>
+						<a href="mailto:#">khairasablon@gmail.com</a><br>
+						<br>
+						234 Hidden Pond Road, Ashland City, TN 37015
+					</p>
+				</div>
+			</div>
+
+		</div> <!-- /row of widgets -->
+	</div>
+</footer>
+
+<footer id="underfooter">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-6 widget">
+				<div class="widget-body">
+					<p>234 Hidden Pond Road, Ashland City, TN 37015 </p>
+				</div>
+			</div>
+
+			<div class="col-md-6 widget">
+				<div class="widget-body">
+					<p class="text-right">
+						Copyright &copy; 2014, Your awesome name here<br>
+						Design: <a href="http://www.gettemplate.com" rel="designer">Initio by GetTemplate</a> </p>
+				</div>
+			</div>
+
+		</div> <!-- /row of widgets -->
+	</div>
+</footer>
+
+	<div class="fpro" style="display: none;">
+		<a class="fpro-close" href="#"><i class="fa fa-times"></i></a>
+		<div class="fpro-intro">See also:</div>
+		<h4 class="fpro-caption"><a target="_blank" href="/pro/progressus.html">Progressus PRO <span>Multipurpose Premium Bootstrap Temlplate</span></a> </h4>
+		<div class="fpro-media">
+			<a target="_blank" href="/pro/progressus.html"><img src="/images/pp-shots.png" alt="Progressus PRO"></a>
+			<div class="fpro-desc">
+				<p>
+					Progressus PRO is a clean and feature-rich Bootstrap theme which will save you a lot of time &amp; effort.
+					<a target="_blank" href="/pro/progressus.html"><b>View Details &rarr;</b></a></p>
+			</div>
+		</div>
+	</div>
+
+
+<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-48366846-1', 'gettemplate.com');
+  ga('send', 'pageview');
+</script>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="assets/js/template.js"></script>
+</body>
+</html>
