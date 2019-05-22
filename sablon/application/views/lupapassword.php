14b6b5a135819807d7e8a94e54157ac6d2b9c311
NoName <arifadi822@gmail.com>
2019-05-22 15:20:58 +0700

    login

---
+++ sablon/application/views/lupapassword.php
@@ -0,0 +1,44 @@
+  <body bgcolor="white">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+
+                <div class="modal-body">
+
+                    <div class="login px-4 mx-auto mw-100">
+                        <h5 class="text-center mb-4">Lupa Password</h5>
+                        <form class="form-signin" method="post">
+                            <div class="form-group">
+                                <label>Nama Lengkap</label>
+
+                                <input type="text" class="form-control" id="validationDefault01" placeholder="" required="">
+                            </div>
+
+                            <div class="form-group">
+                                <label class="mb-2">Kata Sandi Baru</label>
+                                <input type="password" class="form-control" id="password1" placeholder="" required="">
+                            </div>
+                            
+                            <div class="form-group">
+                                <label>Konfirmasi Kata Sandi Baru</label>
+                                <input type="password" class="form-control" id="password2" placeholder="" required="">
+                            </div>
+                                
+                            <div class="form-group">
+                                <label>Email</label>
+                                <input type="email" class="form-control" id="password2" placeholder="" required="">
+                            </div>
+                            <button type="submit" class="btn btn-primary submit mb-4">Ganti Password</button>
+                        </form>
+        
+                    </div>
+                </div>
+
+            </div>
+        </div>
+      <hr>
+    <footer>
+        <p align="center">Tugas Akhir &copy; 2019</p>
+    </footer>
+
+    </div> 
+  </body>
---
+++ sablon/application/views/register.php
@@ -0,0 +1,69 @@
+  <body bgcolor="white">
+    <div role="document">
+
+
+                <div class="modal-body">
+
+                    <div class="login px-4 mx-auto mw-100">
+                        <h2 class="section-title"><span>Register</span></h2>
+                        <form class="form-signin" method="post">
+                            <div class="form-group">
+                                <label for="name">Nama Pengguna</label>
+                                <input type="text" class="form-control" id="validationDefault01" placeholder="Enter Name" name="nama" required="">
+                            </div>
+                            <div class="form-group">
+                                <label>Email</label>
+                                <input type="text" class="form-control" id="validationDefault02" placeholder="Enter Email" required="">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control" id="txtpassword" placeholder="Enter Password" required="">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="re-password">Repeat Password</label>
+                                <input type="password" class="form-control" id="txtconfirmpassword" placeholder="Enter Password" required="">
+                            </div>
+                            
+                            <div class="form-group">
+                                <label>Telepon/HP</label>
+                                <input type="number" class="form-control" name="telepon" placeholder="Telepon/HP" required="">
+                            </div>
+
+                                <div class="form-group">
+                                <label>Alamat</label>
+                                <input type="text" class="form-control" name="alamat" placeholder="Alamat" required="">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="name"><b>Pertanyaan</b></label>
+                                <select name="pertanyaan" class="col-sm-12" style="height: 50px; color: black; margin-top: 30px;">
+                                <option value="Apa Nama Hewan Peliharaan Anda?">Apa Nama Hewan Peliharaan Anda</option>
+                                <option value="Apa Makanan Favorit Anda?">Apa Makanan Favorit Anda?</option>
+                                <option value="Apa Warna Kesukaan Anda?">Apa Warna Kesukaan Anda?</option>
+                                <option value="Apa Nama Buku Favorit Anda?">Apa Nama Buku Favorit Anda?</option></select>
+                            </div>
+
+
+                            <div class="form-group">
+                                <label>Jawaban</label>
+                                <input type="text" class="form-control" name="jawaban" placeholder="jawaban" required="">
+                            </div>
+
+
+                            <button type="submit" class="btn btn-primary submit mb-4">Daftar</button>
+                        </form>
+        
+                    </div>
+                </div>
+
+            </div>
+        </div>
+      <hr>
+    <footer>
+        <p align="center">Tugas Akhir &copy; 2019</p>
+    </footer>
+
+    </div> 
+  </body>
