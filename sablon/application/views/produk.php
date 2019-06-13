eb28e63d1f1e49fd05541f25433511d04ce84d18
Nofita Safira Anggraini <nofitasafira99@gmail.com>
2019-06-13 19:47:09 +0700

    update gambar

--- sablon/application/views/produk.php
+++ sablon/application/views/produk.php
@@ -3,25 +3,25 @@
       <div class="row">
         <div class="col-sm-6 col-md-3">
           <h3 class="text-center">KAOS 1</h3>
-          <img src= <?php echo base_url('assets/images/images.png'); ?>>
+          <img src= <?php echo base_url('assets/images/MEMBUAT-DESAIN-YANG-KEREN-PADA-JASA-SABLON-KAOS-3D-BANDUNG.jpg'); ?>>
           <p>I don't think they tried to market it to the billionairs1.jpge, spelunking, base-jumping crowd. i did the same thing to gandhi, he didn't eat for three weeks. i once heard a wise man say there are no perfect men.</p>
           <p class="text-center"><a href="" class="btn btn-primary">Beli</a></p>
         </div>
         <div class="col-sm-6 col-md-3">
           <h3 class="text-center">KAOS 2</h3>
-          <img src= <?php echo base_url('assets/images/images.png'); ?>>
+          <img src= <?php echo base_url('assets/images/cool_t_shirt_10.jpg'); ?>>
           <p>I don't think they tried to market it to the billionaire, spelunking, base-jumping crowd. i did the same thing to gandhi, he didn't eat for three weeks. i once heard a wise man say there are no perfect men.</p>
           <p class="text-center"><a href="" class="btn btn-primary">Beli</a></p>
         </div>
         <div class="col-sm-6 col-md-3">
           <h3 class="text-center">Kaos 3</h3>
-           <img src= <?php echo base_url('assets/image/s1.jpg.jpg"'); ?> alt"">
+          <img src= <?php echo base_url('assets/images/bakau1.jpg'); ?>>
           <p>I don't think they tried to market it to the billionaire, spelunking, base-jumping crowd. i did the same thing to gandhi, he didn't eat for three weeks. i once heard a wise man say there are no perfect men.</p>
           <p class="text-center"><a href="" class="btn btn-primary">Beli</a></p>
         </div>
         <div class="col-sm-6 col-md-3">
           <h3 class="text-center">Kaos 4</h3>
-           <img src= <?php echo base_url('assets/image/s1.jpg.jpg"'); ?> alt"">
+          <img src= <?php echo base_url('assets/images/cod.jpg'); ?>>
           <p>I don't think they tried to market it to the billionaire, spelunking, base-jumping crowd. i did the same thing to gandhi, he didn't eat for three weeks. i once heard a wise man say there are no perfect men.</p>
           <p class="text-center"><a href="" class="btn btn-primary">Beli</a></p>
         </div>
