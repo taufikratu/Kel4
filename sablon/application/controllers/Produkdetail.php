73ee22390460ed9239486b5c6479d75a1bd6b51d
Nofita Safira Anggraini <nofitasafira99@gmail.com>
2019-07-11 20:38:13 +0700

    zeyeng

--- sablon/application/controllers/Produkdetail.php
+++ sablon/application/controllers/Produkdetail.php
@@ -1,12 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Produkkaos extends CI_Controller {
+class Produkdetail extends CI_Controller {
 
 	public function index()
 	{
 		$this->load->view('header');
-		$this->load->view('produkdetail',$data);
+		$this->load->view('produkdetail');
 		$this->load->view('footer');
 	}
 }
--- sablon/application/views/produkdetail.php
+++ sablon/application/views/produkdetail.php
@@ -21,14 +21,20 @@
 			<aside class="col-md-4 sidebar sidebar-left">
 
 				<div class="widget">
-				<p>Pilih ukuran baju:<br>
-        <input type="radio" name="ukuran" value="s">S (P:62cm L:42cm) <br><input type="label"><br>
-        <input type="radio" name="ukuran" value="m">M (P:66cm L:46cm) <br><input type="label"><br>
-        <input type="radio" name="ukuran" value="l">L (P:70cm L:50cm) <br><input type="label"><br>
-        <input type="radio" name="ukuran" value="XL">XL (P:72cm L:52cm) <input type="label"><br>  
+				<p>Pilih ukuran baju:<br></p>
+      <div class="form-check">
+  			<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="s" checked>S (P:62cm L:42cm) <br></input><br>
+  			<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="m" >M (P:66cm L:46cm) <br></input><br>
+  			<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="l" >L (P:70cm L:50cm) <br></input><br>
+ 			 <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="xl" >XL (P:72cm L:52cm) <br></input><br>
+ 	</div>
+
+  
+	</div>
         <p>Jumlah:<br>
-        <input type="number" name="kuantiti">
-        <p><button class='btn btn-primary'>BELI</button>
+        <input type="number" name="kuantiti" id="kuantiti">
+        <p>
+        	<button class='btn btn-primary' type="submit" name="submit">BELI</button>
         </p>
 				</div>  
 
